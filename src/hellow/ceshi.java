ef3dce531970f05b5df5e75be6f782f9f91a9dd7
lenovo <https://github.com/nuanfeng1/untitled1.git>
2020-08-05 11:03:27 +0800

    冒泡测试更新

---
+++ src/hellow/ceshi.java
@@ -0,0 +1,26 @@
+package hellow;
+
+public class ceshi {
+    public static void main(String[] args){
+        int[] arr= new int[] {43,21,-52,123,75,-89,56,12,-65,12,78,32};
+
+
+        //冒泡排序
+        for (int i = 0; i < arr.length-1; i++)
+        {
+            for(int j = 0;j<arr.length-1-i;j++) {
+                if (arr[j]>arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
