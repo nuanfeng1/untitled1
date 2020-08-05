caa77ddb46a9a9723bd35a855b60a60048c11a29
lenovo <https://github.com/nuanfeng1/untitled1.git>
2020-08-05 10:43:55 +0800

    更新冒泡排序

--- src/day01/maopao.java
+++ src/day01/maopao.java
@@ -2,7 +2,25 @@ package day01;
 
 public class maopao {
     public static void main(String[] args){
-    System.out.println("123");
-    System.out.println("12345");
+        int[] arr= new int[] {43,21,-52,123,75,-89,56,12,-65,12,78,32};
+
+
+        //冒泡排序
+        for (int i = 0; i < arr.length-1; i++)
+        {
+            for(int j = 0;j<arr.length-1-i;j++) {
+                if (arr[j]>arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i]+"\t");
+        }
     }
 }
